9087a585bda5a7e55ab26a9054db89e33df3bfd4
gabrielledelga1 <33862518-gabrielledelga1@users.noreply.replit.com>
2024-05-06 15:18:12 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+
+// import org.junit.jupiter.api.Test;
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.print("olá mundo!");
+    System.out.println("Gabrielle");
+  }
+
+  // @Test
+  // void addition() {
+  //     assertEquals(2, 1 + 1);
+  // }
+}
